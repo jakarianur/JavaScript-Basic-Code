6987ca7b14cfbef880182458ead115c49a868179
Md Jakaria Nur <104752446+mdjakarianur@users.noreply.github.com>
2024-06-13 12:15:52 +0600

    Update 12. JavaScript Statement.js

--- 12. JavaScript Statement.js
+++ 12. JavaScript Statement.js
@@ -4,12 +4,18 @@
 
 <h2>JavaScript Statements</h2>
 
-<p>In HTML, JavaScript statements are executed by the browser.</p>
+<p>JavaScript code blocks are written between { and }</p>
 
-<p id="demo"></p>
+<button type="button" onclick="myFunction()">Click Me!</button>
+
+<p id="demo1"></p>
+<p id="demo2"></p>
 
 <script>
-document.getElementById("demo").innerHTML = "Hello Dolly.";
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Dolly!";
+  document.getElementById("demo2").innerHTML = "How are you?";
+}
 </script>
 
 </body>
